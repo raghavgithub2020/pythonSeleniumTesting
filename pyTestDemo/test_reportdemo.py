508de7f16a82b35a6a48bb01ad8aa09b788d75b2
raghav <amilineni.raghu@gmail.com>
2020-05-17 13:08:22 +0530

    commit changes thru pycharm

--- pyTestDemo/test_reportdemo.py
+++ pyTestDemo/test_reportdemo.py
@@ -1,6 +1,6 @@
-def test():
-#def test_screenshot_on_test_failure(browser):
+#def test():
+def test_screenshot_on_test_failure(browser):
     # driver = webdriver.Firefox()
-    #browser.get("https://google.com")
+    browser.get("https://google.com")
     assert 2 == 2
-    print ("print is successfull")
+    print ("print is successful")
